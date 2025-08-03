f5ef345426f5185b78c679ae60d76af5fb7e6dea
JavaDevMZ <140167480+JavaDevMZ@users.noreply.github.com>
2025-08-03 23:07:18 +0200

    452. Minimum Number of Arrows to Burst Balloons

---
+++ src/main/java/com/javadevmz/leetcode/medium/MinNumOfArrows.java
@@ -0,0 +1,36 @@
+package com.javadevmz.leetcode.medium;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+/**
+ * 452. Minimum Number of Arrows to Burst Balloons
+
+ * There are some spherical balloons taped onto a flat wall that represents the XY-plane.
+ * The balloons are represented as a 2D integer array points where points[i] = [xstart, xend] denotes a balloon
+ * whose horizontal diameter stretches between xstart and xend. You do not know the exact y-coordinates of the balloons.
+
+ * Arrows can be shot up directly vertically (in the positive y-direction) from different points along the x-axis.
+ * A balloon with xstart and xend is burst by an arrow shot at x if xstart <= x <= xend.
+ * There is no limit to the number of arrows that can be shot.
+ * A shot arrow keeps traveling up infinitely, bursting any balloons in its path.
+
+ * Given the array points, return the minimum number of arrows that must be shot to burst all balloons.
+ */
+public class MinNumOfArrows {
+
+    public int findMinArrowShots(int[][] points) {
+        Arrays.sort(points, Comparator.comparingInt(x -> x[0])); //sort by the first element
+        int count = 1;
+        int last = points[0][1]; //track the last shot position
+        for(int i = 0; i<points.length; i++) {
+            if(points[i][0]>last) { //doesn't overlap
+                count++; //one more arrow
+                last = points[i][1];
+            }else {
+                last = Math.min(last, points[i][1]); //reassign to burst both balloons
+            }
+        }
+        return count;
+    }
+}
